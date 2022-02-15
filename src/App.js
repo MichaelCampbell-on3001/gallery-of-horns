00cc4e0932d40952d3471fcde28fd49af215a669
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-14 17:13:19 -0800

    first pass on galler of horns

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import './App.css'; 
+import Header from "./Header.js"
+import Main from "./Main"
+import Footer from "./Footer";
 
 class App extends React.Component {
   render(){
     return (
   <>
-    <header>
-    <h1> Hello World</h1>;
-    </header>
 
-    <main>
+     <Header />
+     <Main />
+     <Footer /> 
+    
 
-    </main>
+    
   </>
     );
   }
---
+++ src/Hornedbeast.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Hornedbeast extends React.Component {
+  render() {
+    return (
+      <article>
+      <h2>title</h2>
+      <image> src="" alt="alt text"</image>
+      <p>description goes here</p>
+      </article>
+    )
+  }
+}
+
+export default Hornedbeast;
