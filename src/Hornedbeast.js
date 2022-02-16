17d849c8ced5fa22243e02166885baeec58a1dd8
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-15 19:54:17 -0800

    votes functionality

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 class Hornedbeast extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes + 1,
+    })
+  }
+
   render() {
     return (
-      <article>
-      <h2>{this.props.title}</h2>
-      <img src={this.props.image_url}/>
+      <article id={this.props.key}>
+      <h2 onClick={this.handleVotes}>{this.props.title}</h2>
+      <img src={this.props.image_url} alt={this.props.description}/>
       <p>{this.props.description}</p>
+      <p>👍: {this.state.waves}</p>
       </article>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -5,10 +5,16 @@ class Main extends React.Component {
   render() {
     let hornedbeast = [];
     
-    this.props.data.forEach(beast => {
+    this.props.data.forEach((beast, index)  => {
          hornedbeast.push(
 
-          <Hornedbeast image_url={beast.image_url} title={beast.title} description={beast.description} horns={beast.horns} keyword={beast.keyword} />
+          <Hornedbeast 
+          key={index}
+          image_url={beast.image_url} 
+          title={beast.title} 
+          description={beast.description} 
+          horns={beast.horns} 
+          keyword={beast.keyword} />
         
 
          )
