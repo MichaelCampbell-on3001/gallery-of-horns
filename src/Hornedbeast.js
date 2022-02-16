3309b30c5019dd2374c7ca8c1fe7311134fa5813
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-15 17:23:32 -0800

    image is rendering

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -4,9 +4,9 @@ class Hornedbeast extends React.Component {
   render() {
     return (
       <article>
-      <h2>title</h2>
-      <image> src="" alt="alt text"</image>
-      <p>description goes here</p>
+      <h2>{this.props.title}</h2>
+      <img src={this.props.image_url}/>
+      <p>{this.props.description}</p>
       </article>
     )
   }
--- src/Main.js
+++ src/Main.js
@@ -4,10 +4,10 @@ import Hornedbeast from "./Hornedbeast";
 class Main extends React.Component {
   render() {
     return (
-      <main>
-       <Hornedbeast />
-       <Hornedbeast />
-      </main>
+      
+       <Hornedbeast image_url="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" title="Uniwahl" description="A unicorn and a narwhal nuzzling their horns"/>
+      // <Hornedbeast />
+
     );
   }
 }
