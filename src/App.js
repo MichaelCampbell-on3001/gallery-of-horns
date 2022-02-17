3836bac9733610ca53b70ebd681cda58c8e75566
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-16 18:22:00 -0800

    broken lab03

--- src/App.js
+++ src/App.js
@@ -4,12 +4,34 @@ import Header from "./Header.js"
 import Main from "./Main"
 import Footer from "./Footer";
 import data from './data.json';
+import Modal from "react-bootstrap";
 
 class App extends React.Component {
+  showModal:true
+
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal:false
+    })
+  }
+
+  handleShowModal = () =>{
+    this.setState({
+      showModal: true
+    })
+  }
+
   render(){
     return (
   <>
      <Header />
+     <Modal show={this.state.showModal} onHide={this.handleCloseModal}>
+       <Modal.Header closeButton>
+         <Modal.Title>{}</Modal.Title>
+       </Modal.Header>
+
+     </Modal>
      <Main data={data}/>
      <Footer />     
   </>
